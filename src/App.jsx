c6858360df19692a1c775fe7cb2b00faf270b3cf
Fajarudin Zakariya <123469989+fajarudinzakariya@users.noreply.github.com>
2023-10-21 15:24:42 +0700

    fix navbar-ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import Nav from "./components/Navbar"
-import Nav from "./page/Nav"
 import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { getTopHeadlinesNews } from "./stores/news/newsAction";
