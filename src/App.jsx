a0a913e07becf7c44c4d579bdffde0cf565a6dd4
Bella621 <92489927+Bella621@users.noreply.github.com>
2023-10-22 09:14:38 +0700

    fitur routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,12 @@
-import Nav from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Nav from "./components/Navbar" 
+import Home from "./page/homePage"
+import Covid_19 from "./page/covid_19"
+import Indonesia from "./page/indonesia"
+import Progaming from "./page/progaming"
+import Save from './page/save'
+import Search from "./page/search"
+import Footer from "./components/Footer"
 import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { getTopHeadlinesNews } from "./stores/news/newsAction";
@@ -34,16 +42,29 @@ const App = () => {
 	}, []);
 
 	return (
+		
 		<div>
-			<Nav />
-			<div className="flex flex-col min-h-screen">
-				<div className="flex flex-col flex-wrap gap-5">
-					{data.map((item, idx) => (
-						<div key={idx}>{item.title}</div>
-					))}
-				</div>
-			</div>
+			<Router>
+				<Nav />
+					<div className="flex flex-col min-h-screen">
+						<div className="flex flex-col flex-wrap gap-5">
+							{data.map((item, idx) => (
+								<div key={idx}>{item.title}</div>
+							))}
+						</div>
+					</div>
+				<Routes>
+					<Route exact path='/' element={<Home/>}/>
+					<Route path='/covid_19' element={<Covid_19/>}/>
+					<Route path='/indonesia' element={<Indonesia/>}/>
+					<Route path='/progaming' element={<Progaming/>}/>
+					<Route path='/save' element={<Save/>}/>
+					<Route path='/search' element={<Search/>}/>
+				</Routes>
+				<Footer/>
+			</Router>
 		</div>
+		
 	);
 };
 
